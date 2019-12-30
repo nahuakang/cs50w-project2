b40c9d5d4c042bc78f3cd24d8f8e866006c3f760
Nahua Kang <kangnahua@gmail.com>
2019-12-30 19:11:56 +0800

    add session and bug explanation

--- application.py
+++ application.py
@@ -6,13 +6,53 @@ from functools import wraps
 from collections import deque
 
 from flask import Flask, redirect, render_template, request, session, url_for
+from flask_session import Session
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "something-unique" #os.getenv("SECRET_KEY")
 app.config["SESSION_PERMANENT"] = True # or session.permanent=True
-socketio = SocketIO(app)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app) # use Flask server-side session instead of default client-side session
+socketio = SocketIO(app, manage_session=False) # give flask-socketio access to Session instead of forking
 
+"""
+    =====NOTE: Example of default Flask session behavior=====
+    Before closing window: {'currentChannel': 'Games', 'username': 'nahua'}
+    Closing window: "GET /socket.io/?EIO=3&transport=websocket&sid=12cdda1286724c849d3c9a4267f24422 HTTP/1.1" 200 0 236.749883
+    After reopening window: <SecureCookieSession {'currentChannel': 'Lounge', 'username': 'nahua'}>
+
+    =====REASON: FLASK-SOCKETIO ACCESS TO FLASK'S CONTEXT GLOBALS=====
+    "A copy of the user session at the time the SocketIO connection is established is
+    made available to handlers invoked in the context of that connection. If a SocketIO
+    handler modifies the session, the modified session will be preserved for future
+    SocketIO handlers, but regular HTTP route handlers will not see these changes.
+    Effectively, when a SocketIO handler modifies the session, a “fork” of the session
+    is created exclusively for these handlers."
+
+    "When using server-side sessions such as those provided by the Flask-Session or
+    Flask-KVSession extensions, changes made to the session in HTTP route handlers can
+    be seen by SocketIO handlers, as long as the session is not modified in the SocketIO handlers."
+
+    =====SOLUTION=====
+    https://blog.miguelgrinberg.com/post/flask-socketio-and-the-user-session
+    ```
+    app.config['SESSION_TYPE'] = 'filesystem'
+    Session(app)
+    socketio = SocketIO(app, manage_session=False)
+    ```
+    With the above configuration, the server will create a subdirectory called flask_session in the
+    current directory and write user sessions for all clients in it. These files will be written to
+    by Flask or by Flask-SocketIO whenever changes to the session are made.
+
+    If you set manage_session=True instead, the user sessions will continue to be forked as described
+    above, regardless of what type of session you use.
+
+    =====BUG=====
+    There is still a bug:
+    If after closing the window, the user directly accesses http://127.0.0.1:5000/create,
+    the user will not be directed to the new channel but instead the one before closing window.
+"""
 
 """GLOBAL VARIABLES"""
 usernames = []
@@ -76,10 +116,6 @@ def socketDebug(fnName, data):
 @app.route("/")
 @login_required
 def index():
-    # NOTE: Example of session behavior
-    # Before closing window: {'currentChannel': 'Games', 'username': 'nahua'}
-    # Closing window: "GET /socket.io/?EIO=3&transport=websocket&sid=12cdda1286724c849d3c9a4267f24422 HTTP/1.1" 200 0 236.749883
-    # After reopening window: <SecureCookieSession {'currentChannel': 'Lounge', 'username': 'nahua'}>
     return redirect(url_for("chat"))
 
 
@@ -157,20 +193,7 @@ def chat():
 def join(data):
     join_room(data['channel'])
 
-    """
-    =====ACCESS TO FLASK'S CONTEXT GLOBALS=====
-    A copy of the user session at the time the SocketIO connection is established is 
-    made available to handlers invoked in the context of that connection. If a SocketIO
-    handler modifies the session, the modified session will be preserved for future
-    SocketIO handlers, but regular HTTP route handlers will not see these changes. 
-    Effectively, when a SocketIO handler modifies the session, a “fork” of the session
-    is created exclusively for these handlers.
-
-    When using server-side sessions such as those provided by the Flask-Session or
-    Flask-KVSession extensions, changes made to the session in HTTP route handlers can
-    be seen by SocketIO handlers, as long as the session is not modified in the SocketIO handlers.
-    """
-    # update socketio session currentChannel; no effect on flask session currentChannel
+    # update socketio session currentChannel; has effect on flask session currentChannel with manage_session=False
     session['currentChannel'] = data['channel']
 
     socketDebug("join", data)
